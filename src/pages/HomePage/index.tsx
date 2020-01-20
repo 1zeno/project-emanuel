31aff31d3c331fb65996dbe3a05a56c110c9bdb9
Emanuel Souza <emanuel-souza1@hotmail.com>
2020-01-20 19:05:50 -0300

    movie

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -18,7 +18,7 @@ import MainLayout from "../../layouts/MainLayout";
 import FilledButton from "../../components/FilledButton";
 
 // Axios para requisição http
-import axios from "axios";
+import Axios from "axios";
 
 // import Movies
 import Movies from "../../components/Movies";
@@ -31,25 +31,23 @@ interface IProps {
 @observer
 export default class HomePage extends React.Component<IProps> {
 
-	state = {
-		movies : []	
-	}
+	public state = {
+		movies : [] 	
+	};
 
-// Requisição get para obter os dados da api	
-	async componentDidMount(){
-		axios.get('https://api.themoviedb.org/3/movie/popular?api_key=323e64a92d60093c30b3a0592469e12b&language=en-US&page=1')
+	// Requisição get para obter os dados da api	
+	public async componentDidMount(){
+		Axios.get('https://api.themoviedb.org/3/movie/popular?api_key=323e64a92d60093c30b3a0592469e12b&language=en-US&page=1')
 		.then(res => this.setState({ movies : res.data.results }))
 	}
 
 	public	render() {
-			return(
-				<MainLayout>
-					<div className="homePageContainer">
-					<Movies movies={this.state.movies} />
-					</div>
-				</MainLayout>
-				
-					
-				);
+		return(
+			<MainLayout>
+				<div className="homePageContainer">
+				<Movies movies={this.state.movies} />
+				</div>
+			</MainLayout>					
+		);
 	}
 }
